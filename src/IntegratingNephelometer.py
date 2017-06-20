d7537e45e8a9febd82e3240425499d7c8cd89775
Austen K. Scruggs <scruggs36mobile@gmail.com>
2017-06-20 17:27:02 -0400

    Add files via upload

---
+++ src/IntegratingNephelometer.py
@@ -0,0 +1,113 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy as sp
+import numpy as np
+import matplotlib.dates as mdates
+import dateutil as du
+from datetime import datetime
+
+
+INeph1 = '/home/austen/PycharmProjects/Integrating Nephelometer/Data/Neph Data/06-19-2017/NL170619Frame.csv'
+
+INEPH = pd.read_csv(INeph1, delimiter=',', header=0)
+
+def SAE_function(sigma1, sigma2, lambda1, lambda2):
+    return -1.0*(np.log(sigma1/sigma2)/np.log(lambda1/lambda2))
+
+
+def SAE(data):
+    df = pd.DataFrame()
+    saebg = []
+    saegr = []
+    cfblue = []
+    cfgreen = []
+    cfred = []
+    timearray = []
+    correctedblue = []
+    correctedgreen = []
+    correctedred = []
+    blue = np.asarray(data.loc[:, 'Blue'])
+    green = np.asarray(data.loc[:, 'Green'])
+    red = np.asarray(data.loc[:, 'Red'])
+    timestamps = np.asarray(data.loc[:, 'Date_Hr'])
+    for counter, element in enumerate(timestamps):
+        time = datetime.strptime(timestamps[counter], "%Y-%m-%d %H:%M:%S")
+        timearray.append(time)
+    timearray = mdates.date2num(timearray)
+    for counter, element in enumerate(timestamps):
+        saevalbg = SAE_function(float(blue[counter]), float(green[counter]), 450.0E-9, 550.0E-9)
+        saevalgr = SAE_function(float(green[counter]), float(red[counter]), 550.0E-9, 700.0E-9)
+        cfblueval = 1.165 + (-.046 * sp.exp(-1.0 * saevalbg))
+        cfgreenval = 1.152 + (-.044 * sp.exp(-1.0 * saevalbg))
+        cfredval = 1.120 + (-.035 * sp.exp(-1.0 * saevalgr))
+        correctedblueval = cfblueval * 1.0E6 * blue[counter]
+        correctedgreenval = cfgreenval * 1.0E6 * green[counter]
+        correctedredval = cfredval * 1.0E6 * red[counter]
+        saebg.append(saevalbg)
+        saegr.append(saevalgr)
+        cfblue.append(cfblueval)
+        cfgreen.append(cfgreenval)
+        cfred.append(cfredval)
+        correctedblue.append(correctedblueval)
+        correctedgreen.append(correctedgreenval)
+        correctedred.append(correctedredval)
+    df['Time Stamp'] = timestamps
+    df['Raw Blue Data'] = blue
+    df['Raw Green Data'] = green
+    df['Raw Red Data'] = red
+    df['S.A.E. (Blue:Green)'] = saebg
+    df['S.A.E. (Green:Red)'] = saegr
+    df['Blue Correction Factor'] = cfblue
+    df['Green Correction Factor'] = cfgreen
+    df['Red Correction Factor'] = cfred
+    df['Corrected Blue Data'] = correctedblue
+    df['Corrected Green Data'] = correctedgreen
+    df['Corrected Red Data'] = correctedred
+    myFmt = mdates.DateFormatter('%Y-%m-%d %H:%M')
+    ax = plt.subplot(211)
+    ax.plot(timearray, saebg, label='SAE Blue:Green')
+    ax.plot(timearray, saegr, label='SAE Green:Red')
+    ax.set_title('Scattering Angstrom Exponent as a Function of Time')
+    ax.set_ylabel('S.A.E.')
+    ax.legend(loc=1)
+    ax.xaxis.set_major_formatter(myFmt)
+    ax1 = plt.subplot(212, sharex=ax)
+    ax1.plot(timearray, correctedblue, 'b-', label='blue wavelength')
+    ax1.plot(timearray, correctedgreen, 'g-', label='green wavelength')
+    ax1.plot(timearray, correctedred, 'r-', label='red wavelength')
+    ax1.set_title('Corrected Scattering Coefficients as a Function of Time')
+    ax1.set_ylabel('Corrected Scattering Coefficient')
+    ax1.tick_params(axis='x', pad=30)
+    ax1.legend(loc=1)
+    plt.setp(ax.get_xticklabels(), visible=False)
+    plt.xticks(rotation=89)
+    plt.gcf().subplots_adjust(bottom=0.30)
+    plt.tight_layout()
+    plt.show()
+    return df
+
+#SAE(INEPH)
+NephDat = SAE(INEPH)
+path = '/home/austen/PycharmProjects/Integrating Nephelometer/Data/Neph Data/06-19-2017/CorrectedData.csv'
+#NephDat.to_csv(path, sep=',', index=True, header=True)
+
+
+
+times = NephDat['Time Stamp'].tolist()
+[str(i) for i in times]
+time_axis = [du.parser.parse(i) for i in times]
+
+
+f, axx = plt.subplots()
+xfmt = mdates.DateFormatter('%Y-%m-%d %H:%M:%S')
+axx.xaxis.set_major_formatter(xfmt)
+axx.plot(time_axis, NephDat['Corrected Blue Data'], 'b-', label='Corrected Blue Coefficients')
+axx.plot(time_axis, NephDat['Corrected Green Data'], 'g-', label='Corrected Green Coefficients')
+axx.plot(time_axis, NephDat['Corrected Red Data'], 'r-', label='Corrected Red Coefficients')
+axx.legend(loc=1)
+axx.set_title('Blue, Green, and Red Corrected Scattering Coefficients as a Function of Time')
+axx.set_xlabel('Time (H:M:S)')
+axx.set_ylabel('Corrected b')
+plt.xticks(rotation=80)
+plt.tight_layout()
+plt.show()
---
+++ src/NephDataParser.R
@@ -0,0 +1,126 @@
+library(shiny)
+require(dplyr)
+require(plyr)
+require(ggplot2)
+require(httr)
+require(RCurl)
+
+    
+    neph <- read.csv(file = "/home/austen/PycharmProjects/Integrating Nephelometer/Data/Neph Data/06-19-2017/NL170619.dat", head = F)%>%
+      filter(V1 == 'D' | V1 =='T')
+    
+    # filter out data with no time value ----
+    # for unknown reasons, some data points are accompanied by a row of time values
+    filt = c()
+    for (i in 1:nrow(neph)) {
+      if (i == nrow(neph)) {
+        rm(i)
+        if (length(filt) == 0) {
+          rm(filt)
+          break()
+        }
+        else {
+          neph = neph[-filt, ]
+          rm(filt)
+          break()
+        }
+      }
+      else {
+        if (neph[i, 1] == neph[(i+1), 1]) {
+          filt <- append(filt, i)
+        }}
+    }
+    
+    # reorder data to give each extinction value a timepoint ----
+    time <- filter(neph, V1 == 'T')
+    neph <- filter(neph, V1 == 'D')
+    neph$date <- as.Date(paste(time$V2, time$V3, time$V4, sep = '-'), format = '%Y-%m-%d')
+    neph$hr   <- time$V5
+    neph$min  <- time$V6
+    
+    # filter zero background points ----
+    neph <- filter(neph, V2 != 'ZBXX') %>%
+      select(4, 5, 6, 12, 13, 14) %>%
+      setNames(c('Blue', 'Green', 'Red', 'Date', 'Hour', 'Min'))
+    neph$Date_Hr <- as.POSIXct(paste(neph$Date, ' ', neph$Hour,':',neph$Min, ':00', sep = ''))
+    
+    # extract desied wavelengths ----
+    neph.plot <- neph[ , c(which(colnames(neph) %in% input$data1)), drop = F]
+    neph.plot <- stack(neph.plot) %>%
+      setNames(c('Scatter', 'Band'))
+    neph.plot$Date_Hr <- neph$Date_Hr
+    color <- c('Red' = 'red', 'Green' = 'forestgreen', 'Blue' = 'blue')[which(c('Red', 'Green', 'Blue') %in% input$data1)]
+    
+    scatter.ymax <- input$scatter.ymax
+    scatter.ymin <- input$scatter.ymin
+    
+    # draw neph plot -----
+    p.neph <- ggplot(aes(x = Date_Hr, y = Scatter, color = Band), data = neph.plot) + 
+      theme_bw() + xlab(' ')  + ylim(scatter.ymin, scatter.ymax) + xlab('Date/Time') +
+      ylab(expression(paste('Sacttering (M',m^-1, ')'))) +
+      geom_line() + 
+      scale_colour_manual(values = color) +
+      theme(legend.position = c(0, 1), legend.justification = c(0, 1))  #+ xlim(xmin, xmax)
+    print(p.neph)
+  
+    neph <- read.csv(file  = "/home/austen/PycharmProjects/Integrating Nephelometer/Data/Neph Data/06-19-2017/NL170619.dat", head = F)%>%
+      filter(V1 == 'D' | V1 =='T')
+    
+    # filter out data with no time value ----
+    # for unknown reasons, some data points are accompanied by a row of time values
+    filt = c()
+    for (i in 1:nrow(neph)) {
+      if (i == nrow(neph)) {
+        rm(i)
+        if (length(filt) == 0) {
+          rm(filt)
+          break()
+        }
+        else {
+          neph = neph[-filt, ]
+          rm(filt)
+          break()
+        }
+      }
+      else {
+        if (neph[i, 1] == neph[(i+1), 1]) {
+          filt <- append(filt, i)
+        }}
+    }
+    
+    # reorder data to give each extinction value a timepoint ----
+    time <- filter(neph, V1 == 'T')
+    neph <- filter(neph, V1 == 'D')
+    neph$date <- as.Date(paste(time$V2, time$V3, time$V4, sep = '-'), format = '%Y-%m-%d')
+    neph$hr   <- time$V5
+    neph$min  <- time$V6
+    
+    # filter zero background points ----
+    neph <- filter(neph, V2 != 'ZBXX') %>%
+      select(4, 5, 6, 12, 13, 14) %>%
+      setNames(c('Blue', 'Green', 'Red', 'Date', 'Hour', 'Min'))
+    neph$Date_Hr <- as.POSIXct(paste(neph$Date, ' ', neph$Hour,':',neph$Min, ':00', sep = ''))
+    
+    # convert from scattering to PM2.5 -----
+    neph$PM_2.5 <- neph$Green / 3.2e-6 - 1.665e-6 #4e5 - 5
+    PM2.5 <- group_by(neph, Date_Hr) %>%
+      select(Date_Hr, PM_2.5)
+    
+    current <- PM2.5[nrow(PM2.5), 1:2]
+    output$current <- renderText({
+      paste(current$Date_Hr, as.character(current$PM_2.5))
+    })
+    
+    
+    #   # set axis limits -----
+    pm.ymax <- input$pm.ymax
+    pm.ymin <- input$pm.ymin
+    
+    # plot PM2.5 values -----
+    p.pm <- ggplot(aes(x = Date_Hr, y = PM_2.5), data = PM2.5) + 
+      theme_bw() + xlab(' ')  + ylim(pm.ymin, pm.ymax) +
+      xlab('Date/Time') +
+      ylab(expression(paste('[P',M[2.5], '] (', mu,'g ', m^-3,')'))) + geom_line(color = 'dodgerblue') #+ xlim(xmin, xmax)
+    print(p.pm)
+    
+    write.csv(neph, file = "/home/austen/PycharmProjects/Integrating Nephelometer/Data/Neph Data/06-19-2017/NL170619Frame.csv", row.names = FALSE)
