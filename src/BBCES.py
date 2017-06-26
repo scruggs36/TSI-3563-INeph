033d45ec271b07772afab609e2b0da272a297743
Austen K. Scruggs <scruggs36mobile@gmail.com>
2017-06-26 17:50:14 -0400

    created BBCES python evaluation file and updated INEPH python evaluation file, added 60/20/2017 data update

--- src/BBCES.py
+++ src/BBCES.py
@@ -6,11 +6,35 @@ import matplotlib.dates as mdates
 import dateutil as du
 from datetime import datetime
 from datetime import timedelta
+from datetime import date
 
-BBCES1Windows ='C:/Users/sm2/Documents/Github Repository Clone/TSI-3563-INeph/Data/06-19-2017/BBCES Data/ext_data2.txt'
+BBCES1Windows = 'C:/Users/sm2/Documents/Github Repository Clone/TSI-3563-INeph/Data/06-19-2017/BBCES Data/ext_data2.txt'
+BBCES2Windows = 'C:/Users/sm2/Documents/Github Repository Clone/TSI-3563-INeph/Data/06-19-2017/BBCES Data/time_data.txt'
+INEPH1Windows = 'C:/Users/sm2/Documents/Github Repository Clone/TSI-3563-INeph/Data/06-19-2017/CorrectedData.csv'
+INEPH2Windows = 'C:/Users/sm2/Documents/Github Repository Clone/TSI-3563-INeph/Data/06-20-2017/CorrectedData.csv'
 
+INEPH1 = pd.read_csv(INEPH1Windows, delimiter=',', header=0)
+INEPH2 = pd.read_csv(INEPH2Windows, delimiter=',', header=0)
+INEPH = pd.concat([INEPH1, INEPH2])
+
+BBCES_ts = pd.read_csv(BBCES2Windows, delimiter='\t', header=0)
+#print(BBCES_ts)
 BBCES = pd.read_csv(BBCES1Windows, delimiter='\t', header=0, engine='python')
+print(BBCES)
+#BBCES['Time String'] = np.asarray(BBCES_ts)
 
+print(BBCES)
+# Interpret time stamps with datetime and re-append as new column in data frame
+INeph_py_time = []
+
+for element in INEPH['Time Stamp']:
+    t = datetime.strptime(element, "%Y-%m-%d %H:%M:%S")
+    INeph_py_time.append(t)
+
+print(INeph_py_time)
+INEPH['PY Time'] = INeph_py_time
+
+# Function to plot raw data obtained from BBCES and Line up time stamps from INEPH and BBCES
 
 def n_point_average(number, data):
     matlab_time_points = np.asarray(data['serial_time'])
@@ -27,6 +51,8 @@ def n_point_average(number, data):
     mean_data2 = []
     for element in matlab_time_points:
         py_time = datetime.fromordinal(int(element)) + timedelta(days=element%1) - timedelta(days = 366)
+        #print(py_time)
+        # printing py_time shows the data is 1 second data
         python_time_points.append(py_time)
     new_data['Python Time'] = python_time_points
     f, axxx = plt.subplots(2, 1, sharex=True)
@@ -44,6 +70,12 @@ def n_point_average(number, data):
     plt.xticks(rotation=80)
     plt.tight_layout()
     plt.show()
+    '''
+    # None of the BBCES and INEPH times match exactly
+    for element in np.asarray(new_data['Python Time']):
+        if element == np.asarray(INEPH['PY Time'])[0]:
+            print(element)
+    '''
     '''
     for counter, element in enumerate(time_points):
         a = counter * number
